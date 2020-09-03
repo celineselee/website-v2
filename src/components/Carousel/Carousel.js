c12e5d0166ec374c29e4222a88057c03730e92d0
Angela Zhang <angelazhangyr@gmail.com>
2020-09-02 23:32:36 -0400

    Saving changes

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Slider from "react-slick";
+import "./carouselStyles.css";
+import { StyledImage } from "./styles";
+
+import l1 from "../../res/images/landing1.jpg";
+import l2 from "../../res/images/landing2.jpg";
+import l3 from "../../res/images/landing3.jpg";
+import l4 from "../../res/images/landing4.jpg";
+import l5 from "../../res/images/landing5.jpg";
+
+var settings = {
+  autoplay: true,
+  dots: true,
+  fade: true,
+};
+class ReactSlickDemo extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Slider {...settings}>
+          <div>
+            <StyledImage src={l1} size="massive" />
+          </div>
+          <div>
+            <StyledImage src={l2} size="massive" />
+          </div>
+          <div>
+            <StyledImage src={l3} size="massive" />
+          </div>
+          <div>
+            <StyledImage src={l4} size="massive" />
+          </div>
+          <div>
+            <StyledImage src={l5} size="massive" />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export default ReactSlickDemo;
+
+// Used https://github.com/kenwheeler/slick/
+// yarn add react-slick
+// npm install slick-carousel
---
+++ src/components/Carousel/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Image } from "semantic-ui-react";
+
+export const StyledImage = styled(Image)`
+  &&& {
+    border-radius: 10px;
+  }
+`;
